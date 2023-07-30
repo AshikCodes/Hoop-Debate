5c4d16d7587e65dd7b88c9c9ce519016f85350ce
Ashik Reji <ashikreji27@gmail.com>
2023-07-30 20:23:17 +0530

    Changed port info

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/new_idea', async (req, res) => {
     }
 })
 
-const PORT = process.env | 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
